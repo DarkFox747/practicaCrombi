4b86c198d195ddec1826d54e7ffce1752d7788e3
DiegoSNotebook <dfsoler13@gmail.com>
2024-11-28 19:34:52 -0300

    db connection

--- webapitests/WebApplication1/WebApplication1/Program.cs
+++ webapitests/WebApplication1/WebApplication1/Program.cs
@@ -1,5 +1,10 @@
 using WebApplication1.Data;
 using WebApplication1.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 var logFilePath = Path.Combine(
     AppDomain.CurrentDomain.BaseDirectory,
@@ -15,6 +20,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<BibliotecaDbContext>();
 builder.Services.AddScoped<IBibliotecaService, BibliotecaService>();
+builder.Services.AddSingleton<SqlConnection>(serviceProvider =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("DefaultConnection");
+    return new SqlConnection(connectionString);
+});
+builder.Services.AddScoped<IDatabaseService, DatabaseService>();
 
 var app = builder.Build();
 
@@ -23,6 +35,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
